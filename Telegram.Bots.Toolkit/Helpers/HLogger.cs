3c7fbecab95b7f3223aabc6d04d76070768b8c87
Azhe <azhe403@gmail.com>
2018-12-06 23:32:26 +0700

    Add logger helper

---
+++ Telegram.Bots.Toolkit/Helpers/HLogger.cs
@@ -0,0 +1,28 @@
+﻿namespace Telegram.Bots.Toolkit.Helpers
+{
+    using NLog;
+    using NLog.Config;
+    using NLog.Targets;
+
+    /// <summary>
+    /// The hn log.
+    /// </summary>
+    internal class HLogger
+    {
+        /// <summary>
+        /// The init.
+        /// </summary>
+        public HLogger()
+        {
+            var config = new LoggingConfiguration();
+
+            var logfile = new FileTarget("logfile") { FileName = "logs.txt" };
+            var logconsole = new ConsoleTarget("logconsole");
+
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            LogManager.Configuration = config;
+        }
+    }
+}
